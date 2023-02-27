7ac3edaf9c5019cc295580a203a3e732ecd537cc
MuchinaWamburia <lucymuchina20@gmail.com>
2023-02-28 00:24:34 +0300

    Done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+      has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+      actors.full_name.each do |actor|
+          "#{actor.&:full_name} - #{actor.show}"
+      end
+    end
 end
